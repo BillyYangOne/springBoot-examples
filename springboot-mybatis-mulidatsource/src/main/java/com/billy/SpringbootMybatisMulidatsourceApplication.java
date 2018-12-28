f336a94622cf78b0320ff6525150f35a61256895
yangle <yangle@kyee.com.cn>
2018-12-28 17:32:41 +0800

    springboot example

---
+++ springboot-mybatis-mulidatsource/src/main/java/com/billy/SpringbootMybatisMulidatsourceApplication.java
@@ -0,0 +1,14 @@
+package com.billy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisMulidatsourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisMulidatsourceApplication.class, args);
+	}
+
+}
+
